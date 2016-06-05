3822daa4985828136bcc4848b03deb466357c201
Max Grinchenko <max@Maxs-MacBook-Air.local>
2016-06-05 11:57:51 -0400

    website ready for content

---
+++ app.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sass= require('node-sass');
+
+var mongo = require('mongodb');
+var monk = require('monk');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+//render sass file
+sass.render({
+  file: 'public/stylesheets/main.scss'
+}, function(err,result) {
+  if (err) {console.log(err); return;}
+  fs.open('public/stylesheets/main.css','w',function(err,fd){
+    if (err) {return;}
+    fs.write(fd,result.css,0,result.css.length,null,function(err,written){
+      if (err) {throw err;}
+      console.log('wrote css file of length '+written+' bytes');
+    })
+
+  })
+  sass = result.css;
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
