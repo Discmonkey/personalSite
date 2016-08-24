873e075bc676556341dc29f42f7ed00f3ef2401f
discmonkey <frismo98@gmail.com>
2016-08-23 21:43:28 -0400

    FIxed error due to removal of database in release version

--- routes/index.js
+++ routes/index.js
@@ -3,20 +3,7 @@ var router = express.Router();
 // select random from mongodb
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var db = req.db;
-	var entries = db.get('entries');
-	var random = Math.random();
-	console.log(random);
-	entries.findOne({'randKey':{'$gt': random}}).on('success', function(doc) {
-		console.log(doc);
-		if ( doc ) {
-			res.render('index', { title: "Max's Website", entry: doc});
-		} else {
-			entries.findOne({'randKey':{'$lt': random}}).on('success', function(doc) {
-				res.render('index', { title: "Max's Website", entry: doc});
-			});
-		}
-	});	
+	res.render('index', { title: "Max's Website"});
 });
 
 module.exports = router;
